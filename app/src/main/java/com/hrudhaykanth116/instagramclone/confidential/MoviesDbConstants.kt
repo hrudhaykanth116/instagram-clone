e58a1cd20ff62d497cc96c85a92902b807c9a73c
Hrudhay Thangella GGK <34122593+hrudhay-thangella@users.noreply.github.com>
2020-06-14 14:09:17 +0530

    Retrieving api key in better way

---
+++ app/src/main/java/com/hrudhaykanth116/instagramclone/confidential/MoviesDbConstants.kt
@@ -0,0 +1,11 @@
+package com.hrudhaykanth116.instagramclone.confidential
+
+import com.hrudhaykanth116.instagramclone.BuildConfig
+
+class MoviesDbConstants {
+
+    companion object{
+        const val API_KEY = BuildConfig.MOVIE_DB_API_KEY
+    }
+
+}

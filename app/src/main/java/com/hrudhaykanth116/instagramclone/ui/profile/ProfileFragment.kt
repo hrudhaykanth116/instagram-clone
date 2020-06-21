b9a3a965477b99960acd52806a1336fad04b0b64
Hrudhay Thangella GGK <34122593+hrudhay-thangella@users.noreply.github.com>
2020-06-21 18:52:57 +0530

    Basic navigation for sign in and sign out

--- app/src/main/java/com/hrudhaykanth116/instagramclone/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/hrudhaykanth116/instagramclone/ui/profile/ProfileFragment.kt
@@ -1,12 +1,17 @@
 package com.hrudhaykanth116.instagramclone.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hrudhaykanth116.instagramclone.R
+import com.hrudhaykanth116.instagramclone.ui.signin.SignInActivity
+import kotlinx.android.synthetic.main.profile_fragment.*
 
 class ProfileFragment : Fragment() {
 
@@ -27,6 +32,20 @@ class ProfileFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        signOutBtn.setOnClickListener {
+            val auth = Firebase.auth
+            auth.signOut()
+            activity?.finishAffinity()
+            val signInIntent = Intent(context, SignInActivity::class.java)
+            signInIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(signInIntent)
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/hrudhaykanth116/instagramclone/ui/signin/SignInActivity.kt
@@ -0,0 +1,75 @@
+package com.hrudhaykanth116.instagramclone.ui.signin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.hrudhaykanth116.instagramclone.MainActivity
+import com.hrudhaykanth116.instagramclone.R
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val firebaseAuth = Firebase.auth
+        if (firebaseAuth.currentUser != null) {
+            navigateToMainActivity()
+        }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        signInBtn.setOnClickListener {
+            signInWithEmailPassword(
+                emailTextField.text.toString(),
+                passwordTextField.text.toString(),
+                firebaseAuth
+            )
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun signInWithEmailPassword(
+        email: String,
+        password: String,
+        firebaseAuth: FirebaseAuth
+    ) {
+        signInBtn.visibility = View.INVISIBLE
+        signInProgressBar.visibility = View.VISIBLE
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d(TAG, "signInWithEmailPassword: Authentication successful.")
+                finishAffinity()
+                navigateToMainActivity()
+            }else{
+                Log.e(TAG, "signInWithEmailPassword: Authentication failure: ${task.exception}")
+                Toast.makeText(this, "Email or Password is incorrect", Toast.LENGTH_SHORT).show()
+                signInBtn.visibility = View.VISIBLE
+                signInProgressBar.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    private fun navigateToMainActivity() {
+        startActivity(Intent(applicationContext, MainActivity::class.java))
+    }
+
+    companion object{
+        private val TAG = SignInActivity.toString()
+    }
+
+}

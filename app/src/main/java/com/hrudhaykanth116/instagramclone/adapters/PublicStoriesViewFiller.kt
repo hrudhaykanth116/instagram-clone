108acc5a13aaa9d3a0f73fa837ad1ae0d6103fea
Hrudhay Thangella GGK <34122593+hrudhay-thangella@users.noreply.github.com>
2020-07-27 02:21:38 +0530

    Added Movie data list as public stories in home fragment

--- app/src/main/java/com/hrudhaykanth116/instagramclone/adapters/PublicStoriesViewFiller.kt
+++ app/src/main/java/com/hrudhaykanth116/instagramclone/adapters/PublicStoriesViewFiller.kt
@@ -2,13 +2,15 @@ package com.hrudhaykanth116.instagramclone.adapters
 
 import com.hrudhaykanth116.instagramclone.viewholders.PublicStatusViewHolder
 
-class PublicStatusViewFiller{
+class PublicStoriesViewFiller{
 
     public fun fillPublicStatusView(publicStatusViewHolder: PublicStatusViewHolder) {
         val testList = ArrayList<String>()
         for (i in 1..20) {
             testList.add("Item: $i")
         }
+
+
         publicStatusViewHolder.publicStoriesContainer.adapter =
             PublicStoryThumbnailAdapter(testList)
     }

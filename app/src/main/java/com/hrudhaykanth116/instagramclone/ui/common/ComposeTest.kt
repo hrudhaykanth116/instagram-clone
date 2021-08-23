66c607209e3c6135f8011246293eb8496e5f910f
hrudhay <hrudhay.kanth@biofourmis.com>
2021-08-24 01:42:42 +0530

    Upgrade AGP - 7.0.0. Added Compose dependenices. Ready to compose.

---
+++ app/src/main/java/com/hrudhaykanth116/instagramclone/ui/common/ComposeTest.kt
@@ -0,0 +1,11 @@
+package com.hrudhaykanth116.instagramclone.ui.common
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Greeting() {
+    Text("Hello Hrudhay")
+}
